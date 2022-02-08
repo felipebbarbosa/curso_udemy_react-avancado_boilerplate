12377b02bf0aaac6d894bedc49fdb36c98a12040
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2022-02-08 20:37:21 -0300

    Configurando o Prettier com o Eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const foo = 'bla'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,9 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Olá mundo!
-        </h1>
+        <h1 className={styles.title}>Olá mundo!</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
